4ccb3859972e3b398be2546f4fc2b68558e04744
Abinesht123 <tabinesh100@gmail.com>
2024-08-04 22:58:31 +0530

    Add files via upload

---
+++ commentlines.dart
@@ -0,0 +1,13 @@
+void main(){
+  //Single line comment
+  print('welcome dart programming');
+  /*Multi line comment
+  print('welcome dart');
+  print('welcome flutter');
+  print('welcome vs studio');
+  */
+  ///documentation commant
+  ///This is dart programming
+  ///flutter application
+  ///vs code editor
+}
