98233634a33e08ae0e9693bd5f9e479ae910ee58
Abinesht123 <tabinesh100@gmail.com>
2024-08-04 22:55:45 +0530

    Add files via upload

---
+++ assignmentoperator.dart
@@ -0,0 +1,14 @@
+void main(){
+  int x,y;
+  x=30;
+  y=20;
+  print('Assignment operator : ${x=y}');
+  print('Add and Assign : ${x+=y}');
+  print('Subtract and Assign : ${x-=y}');
+  print('Multiply and Assign : ${x*=y}');
+  print('Divide and Assign : ${x~/=y}');
+  print('Mod and Assign : ${x%=y}');
+
+
+}
+
