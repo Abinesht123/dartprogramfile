bb72f38fe171611c63083e0d96d5ddc01df6bf9d
Abinesht123 <tabinesh100@gmail.com>
2024-08-04 23:02:37 +0530

    Add files via upload

---
+++ relationaloperator.dart
@@ -0,0 +1,14 @@
+void main(){
+  var a,b;
+  a=12;
+  b=10;
+  print('Greater Than : ${a>b}');
+  print('Less Than : ${a<b}');
+  print('Greater Than Equalto : ${a>=b}');
+  print('Less Than Equalto : ${a<=b}');
+  print('Equality : ${a==b}');
+  print('Not Equal : ${a!=b}');
+}
+
+
+
