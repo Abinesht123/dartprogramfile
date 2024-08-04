6a88ac2dd8d51a601c8459f6ef62f69e979a8f59
Abinesht123 <tabinesh100@gmail.com>
2024-08-04 22:58:57 +0530

    Add files via upload

---
+++ conditionaloperator.dart
@@ -0,0 +1,11 @@
+void main(){
+  var a=5;
+  //?:
+  var result=a>10? 'a is gear than 10':'a is lesser than 10';
+  print(result);
+  var b=20;
+  var c=a>b? a:b;
+  print('The greater numer is ${c}');
+
+}
+
